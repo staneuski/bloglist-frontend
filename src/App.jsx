b1f439fe4e934fc20b5d65742e160a675a832c29
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-12-28 12:03:54 +0200

    5.10: Blog list frontend, step10

--- src/App.jsx
+++ src/App.jsx
@@ -93,13 +93,16 @@ const App = () => {
           {user.name} logged in
           <button onClick={handleLogout}>logout</button>
           <BlogForm createBlog={createBlog} />
-          {blogs.map(blog =>
-            <Blog
-              key={blog.id}
-              blog={blog}
-              handleLike={() => likeBlog(blog.id)}
-            />
-          )}
+          {blogs
+            .sort((lhs, rhs) => rhs.likes - lhs.likes)
+            .map(blog =>
+              <Blog
+                key={blog.id}
+                blog={blog}
+                handleLike={() => likeBlog(blog.id)}
+              />
+            )
+          }
         </>
       }
     </div>
