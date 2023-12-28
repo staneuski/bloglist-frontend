77453b16574fcd969ba83eb5d442395606841c00
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-12-29 01:24:11 +0200

    5.18: bloglist end to end testing, step2

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -10,4 +10,30 @@ describe('Note app', function() {
   it('login form is shown', function() {
     cy.contains('log in').click()
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function () {
+      cy.contains('log in').click()
+      cy.get('[name="username"]').type('JohnDoe')
+      cy.get('[name="password"]').type('Doe')
+      cy.get('.login-form__button-login').click()
+
+      cy.contains('John Doe logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in').click()
+      cy.get('[name="username"]').type('JohnDoe')
+      cy.get('[name="password"]').type('wrong')
+      cy.get('.login-form__button-login').click()
+
+      cy.get('.notification')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'border-style', 'solid')
+      cy.get('.notification_error')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.contains('John Doe logged in').should('not.exist')
+    })
+  })
 })
